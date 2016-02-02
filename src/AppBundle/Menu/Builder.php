efee6169cb35a202fad1e55132a43eed29792531
dmitry <zrayev@mail.ru>
2016-02-02 13:06:34 +0200

    fix bug

--- src/AppBundle/Menu/Builder.php
+++ src/AppBundle/Menu/Builder.php
@@ -13,28 +13,8 @@ class Builder implements ContainerAwareInterface
     public function mainMenu(FactoryInterface $factory, array $options)
     {
         $menu = $factory->createItem('root');
-
         $menu->addChild('Головна', array('route' => 'index'));
         $menu->addChild('Адміністрування', array('route' => 'admin'));
-        $menu->addChild('Автори', array('route' => 'authors'));
-
-
-        // access services from the container!
-//        $em = $this->container->get('doctrine')->getManager();
-//        // findMostRecent and Blog are just imaginary examples
-//        $blog = $em->getRepository('AppBundle:Blog')->findMostRecent();
-//
-//        $menu->addChild('Latest Blog Post', array(
-//            'route' => 'blog_show',
-//            'routeParameters' => array('id' => $blog->getId())
-//        ));
-
-        // create another menu item
-//        $menu->addChild('About Me', array('route' => 'about'));
-//        // you can also add sub level's to your menu's as follows
-//        $menu['About Me']->addChild('Edit profile', array('route' => 'edit_profile'));
-
-        // ... add more children
 
         return $menu;
     }
