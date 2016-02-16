37782942e890ce59f1f8b61bc718ef599e9dedd1
dmitry <zrayev@mail.ru>
2016-02-16 12:01:22 +0200

    fix menu

--- src/AppBundle/Menu/Builder.php
+++ src/AppBundle/Menu/Builder.php
@@ -10,21 +10,23 @@ class Builder implements ContainerAwareInterface
 {
     use ContainerAwareTrait;
 
-    public function mainMenu(FactoryInterface $factory, array $options)
+    public function anonMenu(FactoryInterface $factory)
+    {
+        $menu = $factory->createItem('root');
+        $menu->addChild('menu.home', array('route' => 'index'))->setExtra('translation_domain', 'messages');
+        $menu->addChild('menu.about', array('route' => 'about'))->setExtra('translation_domain', 'messages');
+        $menu->addChild('menu.login', array('route' => 'fos_user_security_login'))->setExtra('translation_domain', 'messages');
+
+        return $menu;
+    }
+
+    public function userMenu(FactoryInterface $factory)
     {
         $menu = $factory->createItem('root');
         $menu->addChild('menu.home', array('route' => 'index'))->setExtra('translation_domain', 'messages');
         $menu->addChild('menu.admin', array('route' => 'admin'))->setExtra('translation_domain', 'messages');
         $menu->addChild('menu.about', array('route' => 'about'))->setExtra('translation_domain', 'messages');
-        $menu->addChild('menu.login', array('route' => 'fos_user_security_login'))->setExtra('translation_domain', 'messages');
         $menu->addChild('menu.logout', array('route' => 'fos_user_security_logout'))->setExtra('translation_domain', 'messages');
-
-//        // will look for "Home" in AppBundle/Resources/translations/AppBundle.locale.yml
-//$menu->addChild('Home', array('route' => 'homepage'))->setExtra('translation_domain', 'AppBundle');
-//
-//// will look for "Login" in Acme/AdminBundle/Resources/translations/AcmeAdminBundle.locale.yml
-//$menu->addChild('Login', array('route' => 'login'))->setExtra('translation_domain', 'AcmeAdminBundle');
-
 
         return $menu;
     }
